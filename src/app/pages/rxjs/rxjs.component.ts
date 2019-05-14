70cfa217c471431139c3345f4698b52107b335f6
Nelson <nquispe@gmail.com>
2019-05-14 08:06:42 -0400

    rxjs

--- src/app/pages/rxjs/rxjs.component.ts
+++ src/app/pages/rxjs/rxjs.component.ts
@@ -1,5 +1,5 @@
-import { Component, OnInit } from '@angular/core';
-import { Observable, Subscriber } from 'rxjs';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Observable, Subscriber, Subscription } from 'rxjs';
 import { retry, map, filter } from 'rxjs/operators';
 
 @Component({
@@ -7,7 +7,9 @@ import { retry, map, filter } from 'rxjs/operators';
   templateUrl: './rxjs.component.html',
   styles: []
 })
-export class RxjsComponent implements OnInit {
+export class RxjsComponent implements OnInit, OnDestroy {
+  suscription: Subscription;
+
 
   constructor() {
     // this.regresaObservable().pipe (
@@ -18,7 +20,7 @@ export class RxjsComponent implements OnInit {
     //    () => console.log('el obs termino!')
     // );
 
-    this.regresaObservable().subscribe (
+    this.suscription = this.regresaObservable().subscribe (
        numero => console.log('subs', numero ),
        error => console.log('error en la obs', error ),
        () => console.log('el obs termino!')
@@ -26,6 +28,12 @@ export class RxjsComponent implements OnInit {
   }
 
   ngOnInit() {
+  }
+
+  ngOnDestroy(){
+    this.suscription.unsubscribe();
+    console.log('la pagina se cerrara');
+    
   }
 
   regresaObservable(): Observable<any> {
@@ -39,10 +47,10 @@ export class RxjsComponent implements OnInit {
         };
 
         observer.next(salida);
-        if (contador === 5) {
-          clearInterval(intervalo);
-          observer.complete();
-        }
+        // if (contador === 5) {
+        //   clearInterval(intervalo);
+        //   observer.complete();
+        // }
         // if (contador === 2) {
         //   //clearInterval(intervalo);
         //   observer.error('dio 2 error help!!!');
